43dc62b6653d8e23f3cea897e58d295726d65f26
akhildevelops <akhilgodvsdemon@gmail.com>
2024-04-25 16:06:42 +0530

    tokenize detokenize

--- src/rank.zig
+++ src/rank.zig
@@ -4,15 +4,17 @@ const fs = std.fs;
 const io = std.io;
 
 pub const TokenRanker = struct {
-    inner: std.StringHashMap(u32),
+    str_to_id: std.StringHashMap(u32),
+    id_to_str: std.HashMap(u32, []const u8, std.hash_map.AutoContext(u32), std.hash_map.default_max_load_percentage),
     allocator: std.mem.Allocator,
     _inner_bucket: std.ArrayList(u8),
     const Self = @This();
-
+    const B = struct { usize, usize, u32 };
     pub fn free(self: *Self) void {
         // Frees up bucket
         self.allocator.free(self._inner_bucket.items);
-        self.inner.clearAndFree();
+        self.str_to_id.clearAndFree();
+        self.id_to_str.clearAndFree();
     }
 
     pub fn from_file(file_path: []const u8, allocator: std.mem.Allocator) !Self {
@@ -26,6 +28,7 @@ pub const TokenRanker = struct {
         defer allocator.free(content);
         return Self.from_string(contentString.str, allocator);
     }
+
     fn _char_vocab(bucket: *std.ArrayList(u8), hashmap: *std.StringHashMap(u32)) !usize {
         var counter: u21 = 0;
         var bucket_pointer: usize = 0;
@@ -44,6 +47,7 @@ pub const TokenRanker = struct {
         }
         return bucket_pointer;
     }
+
     pub fn from_string(content: []const u8, allocator: std.mem.Allocator) !Self {
         var hashmap = std.StringHashMap(u32).init(allocator);
         var bucket = try std.ArrayList(u8).initCapacity(allocator, 50000);
@@ -58,15 +62,67 @@ pub const TokenRanker = struct {
             try hashmap.put(bucket.items[bucket_pointer .. bucket_pointer + line.len - 1], hashmap.count());
             bucket_pointer += line.len - 1;
         }
-        return Self{ .inner = hashmap, ._inner_bucket = bucket, .allocator = allocator };
+        const id_to_str = try utils.revStrHashMap(u32, hashmap, allocator);
+        return Self{ .str_to_id = hashmap, .id_to_str = id_to_str, ._inner_bucket = bucket, .allocator = allocator };
+    }
+    fn _is_present(element: B, index: usize) bool {
+        return element[0] == index;
+    }
+    fn _less_s(_: void, lhs: B, rhs: B) bool {
+        return lhs[0] < rhs[0];
+    }
+    pub fn tokenize(self: Self, data: []const u8, allocator: std.mem.Allocator) ![]const u32 {
+        // _ = self;
+        // _ = data;
+        var temp_arr = std.ArrayList(B).init(allocator);
+        defer temp_arr.clearAndFree();
+        const n_tokens = self.str_to_id.count();
+        for (0..n_tokens) |index| {
+            const kindex = n_tokens - @as(u32, @intCast(index)) - 1;
+            const kv = self.id_to_str.getEntry(kindex).?;
+            var multi_index_iter = utils.multiIndexOf(u8, data, kv.value_ptr.*);
+            while (multi_index_iter.next()) |mindex| {
+                // std.debug.print("{d}:{d}:{s}\n", .{ mindex, kindex, kv.value_ptr.* });
+                var multi_iter = utils.multiIndexOfContext(B, temp_arr.items, mindex, _is_present);
+                // _ = multi_iter;
+                if (multi_iter.next()) |_| {
+                    break;
+                } else {
+                    try temp_arr.append(.{ mindex, kv.value_ptr.*.len, kv.key_ptr.* });
+                }
+            }
+        }
+        std.mem.sort(B, temp_arr.items, {}, _less_s);
+        var tokens = try std.ArrayList(u32).initCapacity(allocator, temp_arr.items.len);
+        for (temp_arr.items) |iitem| {
+            try tokens.append(iitem[2]);
+        }
+        return tokens.toOwnedSlice();
+        // return error.hefwaefh;
+    }
+    pub fn detokenize(self: Self, tokens: []const u32, allocator: std.mem.Allocator) ![]const u8 {
+        var text = std.ArrayList(u8).init(allocator);
+        for (tokens) |token| {
+            try text.appendSlice(self.id_to_str.get(token).?);
+        }
+        return text.toOwnedSlice();
     }
 };
+
+test "TokenRankerBasic" {
+    var tr = try TokenRanker.from_string("", std.testing.allocator);
+    defer tr.free();
+    var iter = tr.str_to_id.iterator();
+    while (iter.next()) |kv| {
+        std.debug.print("{s}:{d}\n", .{ kv.key_ptr.*, kv.value_ptr.* });
+    }
+}
 
 test "TokenRanker" {
     var tr = try TokenRanker.from_string("asdf", std.testing.allocator);
     defer tr.free();
-    try std.testing.expectEqual(tr.inner.get("A").?, 33);
-    try std.testing.expectEqual(tr.inner.count(), 256);
+    try std.testing.expectEqual(tr.str_to_id.get("A").?, 33);
+    try std.testing.expectEqual(tr.str_to_id.count(), 256);
 }
 
 test "TokenRanker partial gpt2" {
@@ -77,13 +133,33 @@ test "TokenRanker partial gpt2" {
         \\Ġbr unch
     , std.testing.allocator);
     defer tr.free();
-    try std.testing.expectEqual(tr.inner.count(), 260);
-    try std.testing.expectEqual(tr.inner.get("Rocket").?, 258);
+    try std.testing.expectEqual(tr.str_to_id.count(), 260);
+    try std.testing.expectEqual(tr.str_to_id.get("Rocket").?, 258);
 }
 
 test "TokenRanker gpt2 from file with partial tokens" {
     var tr = try TokenRanker.from_file("test/gpt2_tokens", std.testing.allocator);
     defer tr.free();
-    try std.testing.expectEqual(tr.inner.count(), 262 + 256);
-    try std.testing.expectEqual(tr.inner.get("Ġme").?, 247 + 255);
+    try std.testing.expectEqual(tr.str_to_id.count(), 262 + 256);
+    try std.testing.expectEqual(tr.str_to_id.get("Ġme").?, 247 + 255);
+}
+
+test "tokenizes" {
+    const allocator = std.testing.allocator;
+    var tr = try TokenRanker.from_file("test/gpt2_tokens", allocator);
+    defer tr.free();
+    const tokens = try tr.tokenize("ousrtr", allocator);
+    defer allocator.free(tokens);
+    const actual: []const u32 = &.{ 516, 385, 83, 82, 84 };
+    std.debug.print("{any}\n", .{tokens});
+    try std.testing.expect(std.mem.eql(u32, tokens, actual));
+}
+
+test "detokenize" {
+    const allocator = std.testing.allocator;
+    var tr = try TokenRanker.from_file("test/gpt2_tokens", allocator);
+    defer tr.free();
+    const text = try tr.detokenize(&.{ 516, 385, 83, 82, 84 }, allocator);
+    defer allocator.free(text);
+    std.debug.print("{s}\n", .{text});
 }
--- src/utils.zig
+++ src/utils.zig
@@ -16,6 +16,83 @@ pub const String = struct {
     }
 };
 
+fn MultiIndexIterator(comptime T: type) type {
+    return struct {
+        start_index: usize,
+        haystack: []const T,
+        needle: []const T,
+
+        const Self = @This();
+        pub fn next(self: *Self) ?usize {
+            const o_index = std.mem.indexOfPos(T, self.haystack, self.start_index, self.needle);
+            if (o_index) |index| {
+                self.start_index = index + self.needle.len - 1;
+            }
+            return o_index;
+        }
+    };
+}
+
+fn MultiIndexContextIterator(comptime T: type, comptime C: type, comptime check: fn (T, C) bool) type {
+    return struct {
+        start_index: usize,
+        haystack: []const T,
+        context: C,
+
+        const Self = @This();
+        pub fn next(self: *Self) ?usize {
+            for (self.start_index..self.haystack.len) |index| {
+                if (check(self.haystack[index], self.context)) {
+                    self.start_index = index + 1;
+                    return index;
+                }
+            }
+            return null;
+        }
+    };
+}
+
+pub fn multiIndexOf(comptime T: type, haystack: []const T, needle: []const T) MultiIndexIterator(T) {
+    return MultiIndexIterator(T){ .start_index = 0, .haystack = haystack, .needle = needle };
+}
+
+pub fn multiIndexOfContext(
+    comptime T: type,
+    haystack: []const T,
+    context: anytype,
+    comptime check: fn (T, @TypeOf(context)) bool,
+) MultiIndexContextIterator(T, @TypeOf(context), check) {
+    return MultiIndexContextIterator(T, @TypeOf(context), check){ .start_index = 0, .haystack = haystack, .context = context };
+}
+fn iseven(x: u8, _: void) bool {
+    return x % 2 == 0;
+}
+test multiIndexOfContext {
+    var m = multiIndexOfContext(u8, &.{ 1, 2, 3, 4, 1, 2, 1 }, {}, iseven);
+    try std.testing.expectEqual(m.next().?, 1);
+    try std.testing.expectEqual(m.next().?, 3);
+    try std.testing.expectEqual(m.next().?, 5);
+    try std.testing.expectEqual(m.next(), null);
+}
+
+test multiIndexOf {
+    var m = multiIndexOf(u8, &.{ 1, 2, 3, 4, 1, 2, 1 }, &.{ 1, 2 });
+    try std.testing.expectEqual(m.next().?, 0);
+    try std.testing.expectEqual(m.next().?, 4);
+    try std.testing.expectEqual(m.next(), null);
+}
+
+pub fn revStrHashMap(comptime V: type, source: std.StringHashMap(V), allocator: std.mem.Allocator) !std.HashMap(V, []const u8, std.hash_map.AutoContext(V), std.hash_map.default_max_load_percentage) {
+    var destination = std.HashMap(V, []const u8, std.hash_map.AutoContext(V), std.hash_map.default_max_load_percentage).init(allocator);
+    var source_iterator = source.iterator();
+
+    while (source_iterator.next()) |kv| {
+        try destination.put(kv.value_ptr.*, kv.key_ptr.*);
+    }
+
+    return destination;
+}
+
 pub fn isPrint(c: u21) error{ CodepointTooLarge, NotYetImplemented }!bool {
     _ = try std.unicode.utf8CodepointSequenceLength(c);
     return switch (c) {
@@ -24,6 +101,22 @@ pub fn isPrint(c: u21) error{ CodepointTooLarge, NotYetImplemented }!bool {
         0xa0...0x1ff => true,
         else => error.NotYetImplemented,
     };
+}
+
+test "revStHashmap" {
+    var source = std.StringHashMap(u32).init(std.testing.allocator);
+    defer source.clearAndFree();
+    try source.put("Hello", 32);
+    try source.put("Hello1", 33);
+    try source.put("Hello2", 34);
+    try source.put("Hello3", 34);
+    var destination = try revStrHashMap(u32, source, std.testing.allocator);
+    defer destination.clearAndFree();
+    try std.testing.expectEqual(destination.count(), 3);
+    try std.testing.expectEqual(destination.get(32).?, "Hello");
+    try std.testing.expectEqual(destination.get(33).?, "Hello1");
+    // put method in hashmap adds keys to stack, therefore when iterating last key comes first i.e, "Hello3" then "Hello2".
+    try std.testing.expectEqual(destination.get(34).?, "Hello2");
 }
 
 test "string_skip_nlines" {
